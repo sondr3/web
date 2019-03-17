d61c437ae298ed1f033016a29805e0cf46baf894
Sondre Nilsen <nilsen.sondre@gmail.com>
2019-03-17 17:06:25 +0100

    Finally fucking fix TS, ESLint, Prettier and everything else

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,27 +1,34 @@
 module.exports = {
   parser: "@typescript-eslint/parser",
+  parserOptions: {
+    ecmaVersion: 2018,
+    sourceType: "module",
+    ecmaFeatures: {
+      jsx: true,
+      impliedStrict: true
+    },
+    project: "./tsconfig.json"
+  },
   env: {
     browser: true,
+    node: true,
     jest: true,
     es6: true
   },
-  parserOptions: {
-    sourceType: "module",
-    ecmaFeatures: {
-      jsx: true
-    }
-  },
-  plugins: ["prettier", "@typescript-eslint", "jest", "react-hooks"],
+  plugins: ["@typescript-eslint", "prettier", "react-hooks", "jest"],
   extends: [
-    "airbnb",
-    "plugin:@typescript-eslint/recommended",
+    "airbnb-base",
+    "eslint:recommended",
     "plugin:react/recommended",
+    "plugin:@typescript-eslint/recommended",
     "prettier",
     "prettier/@typescript-eslint",
     "prettier/react",
     "plugin:jest/recommended"
   ],
   rules: {
+    "react/prop-types": "off",
+    "@typescript-eslint/explicit-function-return-type": "off",
     "prettier/prettier": "error",
     "react-hooks/rules-of-hooks": "error"
   },
