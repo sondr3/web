71511d95afa54167a7585db7652532ddffb2bc8d
Sondre Nilsen <nilsen.sondre@gmail.com>
2019-03-17 21:13:21 +0100

    Add React Helmet

--- packages/portfolio/gatsby-config.js
+++ packages/portfolio/gatsby-config.js
@@ -2,6 +2,7 @@ module.exports = {
   plugins: [
     `gatsby-plugin-typescript`,
     `gatsby-plugin-styled-components`,
+    `gatsby-plugin-react-helmet`,
     {
       resolve: `gatsby-mdx`,
       options: {
