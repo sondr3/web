72d0d121cf879652b62b92ff0e0d8c3197952861
Sondre Nilsen <nilsen.sondre@gmail.com>
2019-03-17 20:55:41 +0100

    Add Stylelint

--- packages/portfolio/src/components/layout.tsx
+++ packages/portfolio/src/components/layout.tsx
@@ -1,9 +1,18 @@
 import React from "react";
+import styled from "styled-components";
 
 interface Props {
   children: React.ReactNode;
 }
 
+const Wrapper = styled.div`
+  margin: 0 auto;
+`;
+
 export default function Layout({ children }: Props) {
-  return <div>{children}</div>;
+  return (
+    <Wrapper>
+      <div>{children}</div>
+    </Wrapper>
+  );
 }
