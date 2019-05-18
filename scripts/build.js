b5c5cc5356f1af81e5b593613e13718da2c1eee2
Sondre Nilsen <nilsen.sondre@gmail.com>
2019-05-19 00:27:50 +0200

    Trial run succeeded, back to 'normal'

--- scripts/build.js
+++ scripts/build.js
@@ -6,13 +6,8 @@ execSync("rm -rf public", { stdio: "inherit" });
 
 const { URL } = process.env;
 
-console.log(`URL: ${URL}`);
-
 switch (URL) {
   case "https://www.eons.io":
-    execSync("yarn workspace portfolio deploy", { stdio: "inherit" });
-    break;
-  case "https://amazing-tereshkova-64abb0.netlify.com":
     execSync("yarn workspace portfolio deploy", { stdio: "inherit" });
     break;
   default:
