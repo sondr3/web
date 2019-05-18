3516257debacff12629044d1904203228c5566c0
Sondre Nilsen <nilsen.sondre@gmail.com>
2019-05-19 00:03:14 +0200

    Simply ESLint and TypeScript configuration

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,36 +1,31 @@
 module.exports = {
-  root: true,
   parser: "@typescript-eslint/parser",
   parserOptions: {
     ecmaVersion: 2018,
     sourceType: "module",
-    ecmaFeatures: {
-      jsx: true,
-      impliedStrict: true
-    },
     project: "./tsconfig.json"
   },
   env: {
     browser: true,
     node: true,
-    jest: true,
     es6: true
   },
-  plugins: ["@typescript-eslint", "prettier", "react-hooks", "jest", "import"],
+  plugins: ["@typescript-eslint", "react-hooks"],
   extends: [
     "airbnb",
     "eslint:recommended",
     "plugin:react/recommended",
     "plugin:@typescript-eslint/recommended",
-    "prettier",
+    "plugin:import/errors",
+    "plugin:import/warnings",
+    "plugin:import/typescript",
+    "plugin:prettier/recommended",
     "prettier/@typescript-eslint",
-    "prettier/react",
-    "plugin:jest/recommended"
+    "prettier/react"
   ],
   rules: {
     "react/jsx-filename-extension": "off",
     "@typescript-eslint/explicit-function-return-type": "off",
-    "prettier/prettier": "error",
     "react-hooks/rules-of-hooks": "error",
     "react-hooks/exhaustive-deps": "warn",
     "import/no-unresolved": "error"
