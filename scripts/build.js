1374678dd152eccc7dfa8160b9ec4477a527123f
Sondre Nilsen <nilsen.sondre@gmail.com>
2019-05-19 00:16:20 +0200

    Trial run of building

--- scripts/build.js
+++ scripts/build.js
@@ -6,11 +6,16 @@ execSync("rm -rf public", { stdio: "inherit" });
 
 const { URL } = process.env;
 
+console.log(`URL: ${URL}`);
+
 switch (URL) {
   case "https://www.eons.io":
     execSync("yarn workspace portfolio deploy", { stdio: "inherit" });
     break;
+  case "https://amazing-tereshkova-64abb0.netlify.com":
+    execSync("yarn workspace portfolio deploy", { stdio: "inherit" });
+    break;
   default:
-    console.error("No SITE_ID specified, exiting...");
+    console.error("No URL specified, exiting...");
     break;
 }
