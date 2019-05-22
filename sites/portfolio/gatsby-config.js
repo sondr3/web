cacf6e3bbfa242a9fd5c1fda8747047b7ddd4ff5
Sondre Nilsen <nilsen.sondre@gmail.com>
2019-05-23 00:37:10 +0200

    Use base SEO component in portfolio

--- sites/portfolio/gatsby-config.js
+++ sites/portfolio/gatsby-config.js
@@ -1,3 +1,15 @@
 module.exports = {
+  siteMetadata: {
+    siteUrl: "https://www.eons.io",
+    title: "Sondre Nilsen",
+    description: `Sondre is a computer security student with a passion for safe and reliable software, open source and programming. Firm believer in applying the KISS principle and the worse is better philosophy. Enjoyer of strongly typed languages, Linux and mechanical keyboards. `,
+    author: {
+      name: "Sondre Nilsen",
+      bio: "I make things"
+    },
+    social: {
+      github: "https://github.com/sondr3"
+    }
+  },
   __experimentalThemes: ["base"]
 };
--- sites/portfolio/src/components/intro.tsx
+++ sites/portfolio/src/components/intro.tsx
@@ -1,11 +1,32 @@
 import React from "react";
+import { SEO } from "base/components";
+import { useStaticQuery, graphql } from "gatsby";
 
 export default function About() {
+  const { site } = useStaticQuery(graphql`
+    query {
+      site {
+        siteMetadata {
+          author {
+            bio
+          }
+          social {
+            github
+          }
+        }
+      }
+    }
+  `);
+
   return (
     <>
+      <SEO />
       <h1>Hello!</h1>
       <h2>I&apos;m Sondre.</h2>
-      <p>I make things.</p>
+      <p>
+        {site.siteMetadata.author.bio}. You can see them on my{" "}
+        <a href={site.siteMetadata.social.github}>GitHub</a>.
+      </p>
     </>
   );
 }
