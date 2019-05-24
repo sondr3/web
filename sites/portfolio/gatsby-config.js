457501cb7db79a8bd18f7e248fc68c8b8c2bbace
Sondre Nilsen <nilsen.sondre@gmail.com>
2019-05-24 23:25:56 +0200

    Remove favicon, not actually needed

--- sites/portfolio/gatsby-config.js
+++ sites/portfolio/gatsby-config.js
@@ -3,7 +3,6 @@ module.exports = {
     siteUrl: "https://www.eons.io",
     title: "Sondre Nilsen",
     description: `Sondre is a computer security student with a passion for safe and reliable software, open source and programming. Firm believer in applying the KISS principle and the worse is better philosophy. Enjoyer of strongly typed languages, Linux and mechanical keyboards. `,
-    favicon: "favicon.ico",
     author: {
       name: "Sondre Nilsen",
       bio: "I make things"
