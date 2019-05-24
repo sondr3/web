1445a9cd66d04bea68add9bf2cf37047382fa531
Sondre Nilsen <nilsen.sondre@gmail.com>
2019-05-24 23:23:38 +0200

    Add manifest configuration to portfolio

--- sites/portfolio/gatsby-config.js
+++ sites/portfolio/gatsby-config.js
@@ -3,6 +3,7 @@ module.exports = {
     siteUrl: "https://www.eons.io",
     title: "Sondre Nilsen",
     description: `Sondre is a computer security student with a passion for safe and reliable software, open source and programming. Firm believer in applying the KISS principle and the worse is better philosophy. Enjoyer of strongly typed languages, Linux and mechanical keyboards. `,
+    favicon: "favicon.ico",
     author: {
       name: "Sondre Nilsen",
       bio: "I make things"
@@ -11,5 +12,14 @@ module.exports = {
       github: "https://github.com/sondr3"
     }
   },
-  __experimentalThemes: ["base"]
+  __experimentalThemes: ["base"],
+  plugins: [
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: "Eons.IO",
+        short_name: "Eons" // eslint-disable-line
+      }
+    }
+  ]
 };
