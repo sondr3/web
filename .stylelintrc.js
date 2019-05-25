d69cebb1692151325a36d6f1a5fe9eb9d7b78712
Sondre Nilsen <nilsen.sondre@gmail.com>
2019-05-26 01:08:29 +0200

    Update stylelint [skip ci]

--- .stylelintrc.js
+++ .stylelintrc.js
@@ -1,7 +1,12 @@
 module.exports = {
-  processors: ["stylelint-processor-styled-components"],
+  plugins: ["stylelint-order"],
   extends: [
     "stylelint-config-recommended",
-    "stylelint-config-styled-components"
-  ]
+    "stylelint-a11y/recommended",
+    "stylelint-prettier/recommended"
+  ],
+  rules: {
+    "order/order": ["custom-properties", "declarations"],
+    "order/properties-alphabetical-order": true
+  }
 };
