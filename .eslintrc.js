51577d548c73df37300eaff7134c16346a8f5d8a
Sondre Nilsen <nilsen.sondre@gmail.com>
2019-05-27 00:36:01 +0200

    Use my own configuration packages [skip ci]

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,42 +1,6 @@
 module.exports = {
-  parser: "@typescript-eslint/parser",
   parserOptions: {
-    ecmaVersion: 2018,
-    sourceType: "module",
     project: "./tsconfig.json"
   },
-  env: {
-    browser: true,
-    node: true,
-    es6: true
-  },
-  plugins: ["@typescript-eslint", "react-hooks"],
-  extends: [
-    "airbnb",
-    "eslint:recommended",
-    "plugin:react/recommended",
-    "plugin:@typescript-eslint/recommended",
-    "plugin:import/errors",
-    "plugin:import/warnings",
-    "plugin:import/typescript",
-    "plugin:prettier/recommended",
-    "prettier/@typescript-eslint",
-    "prettier/react"
-  ],
-  rules: {
-    "react/jsx-filename-extension": "off",
-    "react/prop-types": "off",
-    "@typescript-eslint/explicit-function-return-type": "off",
-    "react-hooks/rules-of-hooks": "error",
-    "react-hooks/exhaustive-deps": "warn",
-    "import/no-unresolved": "error"
-  },
-  settings: {
-    "import/resolver": {
-      typescript: {}
-    },
-    react: {
-      version: "detect"
-    }
-  }
+  extends: ["@sondr3/me", "@sondr3/me/react"]
 };
--- .stylelintrc.js
+++ .stylelintrc.js
@@ -1,12 +1,3 @@
 module.exports = {
-  plugins: ["stylelint-order"],
-  extends: [
-    "stylelint-config-recommended",
-    "stylelint-a11y/recommended",
-    "stylelint-prettier/recommended"
-  ],
-  rules: {
-    "order/order": ["custom-properties", "declarations"],
-    "order/properties-alphabetical-order": true
-  }
+  extends: ["@sondr3/stylelint"]
 };
