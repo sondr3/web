13d5ee79682356b37190abdb32f79a1c5479b67f
Sondre Nilsen <nilsen.sondre@gmail.com>
2019-06-01 21:59:50 +0200

    Remove projects from homepage

--- src/components/intro.tsx
+++ src/components/intro.tsx
@@ -1,7 +1,5 @@
 import React from "react";
 import { useStaticQuery, graphql } from "gatsby";
-import ProjectCard from "./project-card";
-import projectList from "../../content/projects";
 import Wrapper from "./wrapper";
 
 export default function About() {
@@ -28,9 +26,6 @@ export default function About() {
         {site.siteMetadata.author.bio}. You can see them on my{" "}
         <a href={site.siteMetadata.social.github}>GitHub</a>.
       </p>
-      {projectList.map(project => (
-        <ProjectCard key={project.id} {...project} />
-      ))}
     </Wrapper>
   );
 }
