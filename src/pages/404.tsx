3ea073c4bc813216ca02bfa1ecc8da31861330b5
Sondre Nilsen <nilsen.sondre@gmail.com>
2019-06-01 22:16:49 +0200

    Create a very simple 404 page

---
+++ src/pages/404.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Layout from "../components/layout";
+import Wrapper from "../components/wrapper";
+
+export default function Index() {
+  return (
+    <Layout>
+      <Wrapper>
+        <h1>404: Page not found</h1>
+        <p>Oh no :(</p>
+      </Wrapper>
+    </Layout>
+  );
+}
