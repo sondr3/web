d2ff03b1941649e5936076934cb130f8022467de
Sondre Nilsen <nilsen.sondre@gmail.com>
2019-06-02 17:59:06 +0200

    Refactor components into more folders based on usage

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,6 @@
 import React from "react";
-import Layout from "../components/layout";
-import Intro from "../components/intro";
+import Layout from "../components/common/layout";
+import Intro from "../components/intro/intro";
 
 export default function Index() {
   return (
