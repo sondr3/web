e96cf141fdc1a5de8a288e769e42aa20e79396d6
Sondre Nilsen <nilsen.sondre@gmail.com>
2019-06-02 21:01:38 +0200

    Create about me section in intro

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,13 @@
 import React from "react";
 import Layout from "../components/common/layout";
 import Intro from "../components/intro/intro";
+import About from "../components/intro/about";
 
 export default function Index() {
   return (
     <Layout>
       <Intro />
+      <About />
     </Layout>
   );
 }
