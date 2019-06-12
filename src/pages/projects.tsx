2515d4c706ef977f503b7a01fe80f00478b374fb
Sondre Nilsen <nilsen.sondre@gmail.com>
2019-06-12 20:50:01 +0200

    Create placeholder pages for projects and blog posts

---
+++ src/pages/projects.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Layout from "../components/common/layout";
+import Wrapper from "../components/common/wrapper";
+
+export default function Projects() {
+  return (
+    <Layout>
+      <Wrapper>
+        <h1>Projects</h1>
+      </Wrapper>
+    </Layout>
+  );
+}
