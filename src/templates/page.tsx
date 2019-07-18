443c108c96a5896da33d2b65e8f2e162ba0c7924
Sondre Nilsen <nilsen.sondre@gmail.com>
2019-07-18 23:45:48 +0200

    Upgrade to gatsby-plugin-mdx, refactor accordingly

--- src/templates/page.tsx
+++ src/templates/page.tsx
@@ -1,32 +1,43 @@
 import React from "react";
-import { useStaticQuery, graphql } from "gatsby";
-import MDXRenderer from "gatsby-mdx/mdx-renderer";
+import { graphql } from "gatsby";
+import MDXRenderer from "gatsby-plugin-mdx/mdx-renderer";
 import SEO from "../components/common/seo";
 import Layout from "../components/common/layout";
 
-export default function PageTemplate() {
-  const { mdx: page } = useStaticQuery(graphql`
-    query PageQuery($id: String) {
-      mdx(id: { eq: $id }) {
-        id
-        frontmatter {
-          title
-          description
-        }
-        code {
-          body
-        }
-      }
-    }
-  `);
+interface PageQueryData {
+  data: {
+    mdx: {
+      id: number;
+      frontmatter: {
+        title: string;
+        description: string;
+      };
+      body: string;
+    };
+  };
+}
 
+export default function PageTemplate({ data: { mdx } }: PageQueryData) {
   return (
     <Layout>
       <SEO
-        title={page.frontmatter.title}
-        description={page.frontmatter.description}
+        title={mdx.frontmatter.title}
+        description={mdx.frontmatter.description}
       />
-      <MDXRenderer>{page.code.body}</MDXRenderer>
+      <MDXRenderer>{mdx.body}</MDXRenderer>
     </Layout>
   );
 }
+
+export const pageQuery = graphql`
+  query PageQuery($id: String) {
+    mdx(id: { eq: $id }) {
+      id
+      frontmatter {
+        title
+        description
+      }
+      body
+    }
+  }
+`;
