391c1672ed9a24f966d3c1a93a60674342d690cc
Sondre Nilsen <nilsen.sondre@gmail.com>
2019-09-01 18:47:59 +0200

    feat: render projects in projects page

--- src/pages/projects.tsx
+++ src/pages/projects.tsx
@@ -1,12 +1,17 @@
 import React from "react";
 import Layout from "../components/common/layout";
 import SEO from "../components/common/seo";
+import ProjectCard from "../components/intro/project-card";
+import projects from "../../content/projects";
 
 const Projects: React.FC = () => {
   return (
     <Layout>
       <SEO title="Projects" />
       <h1>Projects</h1>
+      {projects.map(project => (
+        <ProjectCard key={project.id} {...project} />
+      ))}
     </Layout>
   );
 };
