552649e91a278515d7a95a36506355f47fe6b6d2
Sondre Nilsen <nilsen.sondre@gmail.com>
2019-09-21 00:48:09 +0200

    feat: add featured projects to homepage

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,12 +2,14 @@ import React from "react";
 import Layout from "../components/common/layout";
 import Intro from "../components/intro/intro";
 import About from "../components/intro/about";
+import FeaturedProjects from "../components/intro/featured-projects";
 
 const Index: React.FC = () => {
   return (
     <Layout>
       <Intro />
       <About />
+      <FeaturedProjects />
     </Layout>
   );
 };
