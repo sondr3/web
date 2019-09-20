38bb5ed760517789e9be54093b10daf777e3acdd
Sondre Nilsen <nilsen.sondre@gmail.com>
2019-09-20 21:19:33 +0200

    fix: make intro flow better

--- src/components/intro/intro.tsx
+++ src/components/intro/intro.tsx
@@ -1,25 +1,65 @@
 import React from "react";
 import styled from "styled-components";
 import useAuthorMetadata from "../../hooks/useAuthorMetadata";
+import c from "../../styles/constants";
 
 import dev from "../../../assets/developer.svg";
 
-const Img = styled.img`
+const Portrait = styled.img`
+  max-width: 100%;
   width: 100%;
+`;
+
+const Wrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  flex-wrap: nowrap;
+  height: 100vh;
+  justify-content: space-around;
+  width: 100%;
+`;
+
+const About = styled.section`
+  display: flex;
+  flex-direction: column;
+  justify-content: end;
+
+  @media screen and (min-width: ${c.size.smallWidth}px) {
+    flex-direction: row;
+  }
+`;
+
+const Hello = styled.div`
+  align-items: center;
+  display: flex;
+  flex-direction: column;
+  justify-content: end;
+
+  @media screen and (min-width: ${c.size.smallWidth}px) {
+    width: 100%;
+  }
 `;
 
 const Intro: React.FC = () => {
   const meta = useAuthorMetadata();
 
   return (
-    <>
-      <Img src={dev} alt="Hello! I'm Sondre." />
-      <h1>Hello!</h1>
-      <h2>I&apos;m Sondre.</h2>
-      <p>
-        {meta.author.intro}. You can see them on my <a href={meta.social.github}>GitHub</a>.
-      </p>
-    </>
+    <Wrapper>
+      <About>
+        <Hello>
+          <h1>Hello!</h1>
+          <h2>I&apos;m Sondre.</h2>
+        </Hello>
+        <div>
+          <Portrait src={dev} alt={meta.author.intro} />
+        </div>
+      </About>
+      <section>
+        <p>
+          {meta.author.intro}. You can see them on my <a href={meta.social.github}>GitHub</a>.
+        </p>
+      </section>
+    </Wrapper>
   );
 };
 
