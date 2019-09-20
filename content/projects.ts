ed63d3b368e5b8c6efb3e3cbfe250146b438b4ba
Sondre Nilsen <nilsen.sondre@gmail.com>
2019-09-20 21:18:33 +0200

    feat: add featured projects

--- content/projects.ts
+++ content/projects.ts
@@ -2,6 +2,7 @@ export interface Project {
   id: number;
   name: string;
   description: string;
+  featured: boolean;
   technologies: string[];
   github: string;
 }
@@ -12,6 +13,7 @@ export const projects: { [type: string]: Project[] } = {
       id: 0,
       name: "git ignore",
       description: "A small and simple .gitignore generator",
+      featured: true,
       technologies: ["Rust"],
       github: "https://github.com/sondr3/git-ignore/"
     },
@@ -19,6 +21,7 @@ export const projects: { [type: string]: Project[] } = {
       id: 1,
       name: "git anger management",
       description: "Ever wanted to know just how angry your commits are?",
+      featured: true,
       technologies: ["Rust", "Git"],
       github: "https://github.com/sondr3/git-anger-management/"
     },
@@ -26,6 +29,7 @@ export const projects: { [type: string]: Project[] } = {
       id: 2,
       name: "MOCCA: MOCCA Operational Controller for Coffee Availability",
       description: "How much coffee is left?",
+      featured: true,
       technologies: ["React", "Python", "Django", "Docker", "Arduino", "C++"],
       github: "https://github.com/inf219-mocca/"
     },
@@ -33,6 +37,7 @@ export const projects: { [type: string]: Project[] } = {
       id: 3,
       name: "dotfiles",
       description: "These are my dotfiles, there are many like them but these ones are mine...",
+      featured: false,
       technologies: ["Nix", "NixOS"],
       github: "https://github.com/sondr3/dotfiles/"
     },
@@ -40,6 +45,7 @@ export const projects: { [type: string]: Project[] } = {
       id: 4,
       name: "web",
       description: "My personal webpage",
+      featured: false,
       technologies: ["Gatsby", "React", "Styled Components"],
       github: "https://github.com/sondr3/web/"
     },
@@ -47,6 +53,7 @@ export const projects: { [type: string]: Project[] } = {
       id: 5,
       name: "frontend-config",
       description: "Because I hate configuration",
+      featured: false,
       technologies: ["Lerna", "CI/CD", "JavaScript"],
       github: "https://github.com/sondr3/frontend-config/"
     },
@@ -55,6 +62,7 @@ export const projects: { [type: string]: Project[] } = {
       name: "replieswithtime",
       description:
         "A silly little Twitter bot that replies with the current time... useful for those times when you have Twitter available but no clock.",
+      featured: false,
       technologies: ["Python", "Docker"],
       github: "https://github.com/sondr3/replieswithtime/"
     }
@@ -64,6 +72,7 @@ export const projects: { [type: string]: Project[] } = {
       id: 1,
       name: "Nixpkgs (NixOS)",
       description: "Nix package manager repository.",
+      featured: false,
       technologies: ["Nix", "NixOS", "Rust"],
       github: "https://github.com/NixOS/nixpkgs"
     },
@@ -71,6 +80,7 @@ export const projects: { [type: string]: Project[] } = {
       id: 2,
       name: "Fagkveld.it",
       description: "Nettside for fagkveld",
+      featured: false,
       technologies: ["React", "Styled Components", "GitHub Pages"],
       github: "https://github.com/echo-uib/Fagkveld"
     }
