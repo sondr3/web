d750c0d8cfd76e2d532e2f8df97d8233f5fb895e
Sondre Nilsen <nilsen.sondre@gmail.com>
2019-12-17 21:21:09 +0100

    chore: update and regenerate dependencies, add ESLint-mdx

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,7 +2,7 @@ module.exports = {
   parserOptions: {
     project: "./tsconfig.json"
   },
-  extends: ["@sondr3/typescript", "@sondr3/react", "@sondr3/react/typescript"],
+  extends: ["@sondr3/typescript", "@sondr3/react", "@sondr3/react/typescript", "plugin:mdx/recommended"],
   rules: {
     "@typescript-eslint/strict-boolean-expressions": ["off"],
     "@typescript-eslint/no-unnecessary-condition": ["error", { ignoreRhs: true }]
