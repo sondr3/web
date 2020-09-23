a903b28724a2405b83b79878ef90db049302212d
Sondre Nilsen <nilsen.sondre@gmail.com>
2020-09-24 00:26:57 +0200

    Update packages, fix eslint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,6 +1,8 @@
 module.exports = {
   parserOptions: {
+    ecmaVersion: 2020,
     project: "./tsconfig.json",
+    sourceType: "module",
   },
-  extends: ["@sondr3/typescript"],
+  extends: ["@sondr3/typescript", "plugin:node/recommended-module"],
 };
