45fdcc4f5de1c101be7bd6e23b3256e2842e4af7
Sondre Nilsen <nilsen.sondre@gmail.com>
2020-09-24 17:04:31 +0200

    Create Duration class, tests

---
+++ lib/utils/Duration.spec.ts
@@ -0,0 +1,39 @@
+import { Duration, prettyPrintDuration } from "./Duration";
+
+describe("Duration", () => {
+  const spy = jest.spyOn(process.hrtime, "bigint");
+  it("Returns a formatted duration", () => {
+    spy.mockReturnValue(191051479007711n);
+
+    const duration = new Duration();
+
+    spy.mockReturnValue(191052633396993n);
+    duration.end();
+
+    expect(duration.result()).toBe("1s 154ms");
+  });
+});
+
+describe("PrettyPrintDuration", () => {
+  it("Pretty formats duration, less than 1 second", () => {
+    const duration = 683;
+    const formatted = prettyPrintDuration(duration);
+
+    expect(typeof formatted).toBe("string");
+    expect(formatted).toBe("683ms");
+  });
+
+  it("Pretty formats duration, less than 100ms", () => {
+    const duration = 85;
+    const formatted = prettyPrintDuration(duration);
+
+    expect(formatted).toBe("85ms");
+  });
+
+  it("Pretty formats duration, more than 1 second", () => {
+    const duration = 1632;
+    const formatted = prettyPrintDuration(duration);
+
+    expect(formatted).toBe("1s 632ms");
+  });
+});
