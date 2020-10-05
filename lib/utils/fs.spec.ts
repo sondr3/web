5472910087b4885552429d88c478199ffd2d67e0
Sondre Nilsen <nilsen.sondre@gmail.com>
2020-10-06 01:29:40 +0200

    Add createDirectory function with purify-ts

--- lib/utils/fs.spec.ts
+++ lib/utils/fs.spec.ts
@@ -1,4 +1,4 @@
-import { dirWalk } from "./fs";
+import { createDirectory, dirWalk } from "./fs";
 
 describe("dirWalk", () => {
   it("JSON files without recursing", async () => {
@@ -12,5 +12,19 @@ describe("dirWalk", () => {
 
     expect(files).toContain("lib/utils/fs.ts");
     expect(files).toContain("lib/utils/fs.spec.ts");
+  });
+});
+
+describe("createDirectory", () => {
+  it("can create a test directory", async () => {
+    const res = await createDirectory("/tmp/testing").run();
+    expect(res.isRight()).toBeTruthy();
+  });
+
+  it("cannot create a root directory", async () => {
+    const res = await createDirectory("/test").run();
+    expect(res.isLeft()).toBeTruthy();
+    expect(res.isRight()).toBeFalsy();
+    expect(res.map((r) => r.includes("Could not create directory"))).toBeTruthy();
   });
 });
--- lib/utils/fs.ts
+++ lib/utils/fs.ts
@@ -1,6 +1,6 @@
 import { promises as fs } from "fs";
 import path from "path";
-import { Either, EitherAsync } from "purify-ts";
+import { EitherAsync, liftPromise } from "purify-ts/EitherAsync";
 
 /**
  * Recursively walk directories finding all files matching the extension.
@@ -32,17 +32,7 @@ export async function dirWalk(
   return filepaths;
 }
 
-export const createDirectory = async (filepath: string): Promise<Either<Error, void>> =>
-  EitherAsync(async ({ throwE }) => {
-    try {
-      await fs.mkdir(filepath, { recursive: true });
-    } catch (e) {
-      const err = e as NodeJS.ErrnoException;
-
-      throwE(Error(`Could not create directory: ${err.message}`));
-    }
-  });
-
-createDirectory("/test")
-  .then((res) => console.log(res))
-  .catch((err) => console.error(err));
+export const createDirectory = (filepath: string): EitherAsync<Error, string> =>
+  liftPromise(() => fs.mkdir(filepath, { recursive: true })).mapLeft(({ message }) =>
+    Error(`Could not create directory: ${message}`),
+  );
