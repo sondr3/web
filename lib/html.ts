7fd9fa056145b840751b314717a78c91033cfbf8
Sondre Nilsen <nilsen.sondre@gmail.com>
2020-10-14 16:52:26 +0200

    Add back html tag now that I somewhat know what I'm doing

---
+++ lib/html.ts
@@ -0,0 +1,24 @@
+/**
+ * My handrolled JSX-like solution to templating. Its gloriously awful.
+ *
+ * @param input - Some HTML
+ * @param values - Values that are coerced to strings
+ */
+export const html = (input: TemplateStringsArray, ...values: unknown[]): string => {
+  let output = "";
+  input.forEach((string, i) => {
+    const value = values[i];
+
+    if (Array.isArray(value)) {
+      output += string.toString() + value.join("");
+    } else if (typeof value === "string") {
+      output += string.toString() + value;
+    } else if (typeof value === "number") {
+      output += string.toString() + value.toString();
+    } else {
+      output += string;
+    }
+  });
+
+  return output;
+};
