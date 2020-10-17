465f7f80264906a831f6817ca7eb58a5f6d7b59a
Sondre Nilsen <nilsen.sondre@gmail.com>
2020-10-17 17:35:55 +0200

    Add TSDoc ESlint plugin, TypeDoc to generate documentation

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,5 +4,9 @@ module.exports = {
     project: "./tsconfig.json",
     sourceType: "module",
   },
+  plugins: ["eslint-plugin-tsdoc"],
   extends: ["@sondr3/typescript", "plugin:node/recommended-module", "plugin:jest/recommended", "plugin:jest/style"],
+  rules: {
+    "tsdoc/syntax": "warn",
+  },
 }
