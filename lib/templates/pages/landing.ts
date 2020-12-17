0a26165dac80b2ef97a3fff4371976fb68478a43
Sondre Nilsen <nilsen.sondre@gmail.com>
2020-12-18 00:43:29 +0100

    Update landing page

--- lib/templates/pages/landing.ts
+++ lib/templates/pages/landing.ts
@@ -1,10 +1,26 @@
+import { getConfig } from "../../config"
 import { html } from "../../html"
 import { layout } from "../layouts"
 
+const {
+  meta: { author },
+} = getConfig()
+
 const landingPage = html`
-  <div>
-    <h1>Hello!</h1>
-  </div>
+  <section class="intro">
+    <div class="about">
+      <div class="hello">
+        <h1>Hello!</h1>
+        <h2>I&apos;m Sondre.</h2>
+      </div>
+      <div>
+        <img class="portrait" src="/static/developer.svg" alt="${author.intro}" />
+      </div>
+    </div>
+    <section>
+      <p>${author.intro}. You can see them on my <a href="${author.socials.github}">GitHub</a>.</p>
+    </section>
+  </section>
 `
 
 export const landing = (): string => {
