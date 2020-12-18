da60964385ca5dff581f5023030f2939d7835260
Sondre Nilsen <nilsen.sondre@gmail.com>
2020-12-18 18:54:08 +0100

    Add types for "stream/promises" as its not yet in @types/node

--- lib/types.d.ts
+++ lib/types.d.ts
@@ -35,3 +35,44 @@ declare module "@minify-html/js" {
    */
   export function minifyInPlace(code: Buffer, cfg: Cfg): Buffer
 }
+
+declare module "stream/promises" {
+  import { FinishedOptions } from "stream"
+
+  namespace promises {
+    function finished(
+      stream: NodeJS.ReadableStream | NodeJS.WritableStream | NodeJS.ReadWriteStream,
+      options?: FinishedOptions,
+    ): Promise<void>
+
+    function pipeline(stream1: NodeJS.ReadableStream, stream2: NodeJS.WritableStream): Promise<void>
+    function pipeline(
+      stream1: NodeJS.ReadableStream,
+      stream2: NodeJS.ReadWriteStream,
+      stream3: NodeJS.WritableStream,
+    ): Promise<void>
+    function pipeline(
+      stream1: NodeJS.ReadableStream,
+      stream2: NodeJS.ReadWriteStream,
+      stream3: NodeJS.ReadWriteStream,
+      stream4: NodeJS.WritableStream,
+    ): Promise<void>
+    function pipeline(
+      stream1: NodeJS.ReadableStream,
+      stream2: NodeJS.ReadWriteStream,
+      stream3: NodeJS.ReadWriteStream,
+      stream4: NodeJS.ReadWriteStream,
+      stream5: NodeJS.WritableStream,
+    ): Promise<void>
+    function pipeline(
+      streams: ReadonlyArray<NodeJS.ReadableStream | NodeJS.WritableStream | NodeJS.ReadWriteStream>,
+    ): Promise<void>
+    function pipeline(
+      stream1: NodeJS.ReadableStream,
+      stream2: NodeJS.ReadWriteStream | NodeJS.WritableStream,
+      ...streams: Array<NodeJS.ReadWriteStream | NodeJS.WritableStream>
+    ): Promise<void>
+  }
+
+  export = promises
+}
