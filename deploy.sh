6a0b5e18be7a68ef7bac4e6dfea5a92227f2d85e
Sondre Nilsen <nilsen.sondre@gmail.com>
2020-12-19 16:09:38 +0100

    Fix deploy script not copying files on

--- deploy.sh
+++ deploy.sh
@@ -5,5 +5,11 @@ echo "Deploying website"
 echo "Building new container"
 docker build -t web .
 
+echo "Create tag"
+docker stop web
+docker container rm web
+docker run -itd --name web web
+
 echo "Copying built files"
-docker cp web:/usr/src/web/public ../public
+rm -rf ../public
+docker cp web:/usr/src/web/public ../
