b86722ba9b748e3e38c8b7bdca98ec9312024ad1
Sondre Nilsen <nilsen.sondre@gmail.com>
2021-04-22 17:16:12 +0200

    Add about page

---
+++ pages/about.tsx
@@ -0,0 +1,21 @@
+import { Content } from "../components/content"
+import { Layout } from "../components/layout"
+
+export default function about(): JSX.Element {
+  return (
+    <Layout>
+      <Content header="About me">
+        <p>
+          Hello, I’m Sondre! I’m a master student on my first year studying algorithms at the{" "}
+          <a href="https://www.uib.no/">University of Bergen</a> with a passion for safe and reliable software, open
+          source and programming. I’m also a firm believer in applying the KISS principle and the worse is better
+          philosophy to my projects. I enjoy and use strongly typed languages, Linux and mechanical keyboards.
+        </p>
+        <h2>Contact me</h2>
+        <p>
+          If you want to reach out, my email is <code>my-first-name</code> at this domain.
+        </p>
+      </Content>
+    </Layout>
+  )
+}
