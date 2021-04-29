0a7a7ea69201668f6736af8af9eacbeab61363cd
Sondre Nilsen <nilsen.sondre@gmail.com>
2021-04-29 23:16:26 +0200

    Add compress script to compress built files with gzip/brotli

---
+++ scripts/compress.mjs
@@ -0,0 +1,59 @@
+import { createBrotliCompress, createGzip } from "zlib"
+import fs, { createReadStream, createWriteStream } from "fs"
+import path from "path"
+
+const INVALID_EXT = [".map", ".txt", ".scss", ".gz", ".br", ""]
+
+export function readdirRecursive(directory, ignored_extension, filepaths = []) {
+  const files = fs.readdirSync(directory)
+
+  for (const filename of files) {
+    const filepath = path.join(directory, filename)
+    const stat = fs.statSync(filepath)
+
+    if (stat.isDirectory()) {
+      readdirRecursive(filepath, ignored_extension, filepaths)
+    } else if (!ignored_extension.includes(path.extname(filename))) {
+      filepaths.push(filepath)
+    }
+  }
+
+  return filepaths
+}
+
+function gzip() {
+  const files = readdirRecursive(path.join(process.cwd(), "public"), INVALID_EXT)
+
+  for (const file of files) {
+    const source = createReadStream(file)
+    const destination = createWriteStream(`${file}.gz`)
+    const gzip = createGzip({ level: 9 })
+    source
+      .pipe(gzip)
+      .on("error", (err) => console.error(err))
+      .pipe(destination)
+      .on("error", (err) => console.error(err))
+  }
+}
+
+function brotli() {
+  const files = readdirRecursive(path.join(process.cwd(), "public"), INVALID_EXT)
+
+  for (const file of files) {
+    const source = createReadStream(file)
+    const destination = createWriteStream(`${file}.br`)
+    const brotli = createBrotliCompress()
+    source
+      .pipe(brotli)
+      .on("error", (err) => console.error(err))
+      .pipe(destination)
+      .on("error", (err) => console.error(err))
+  }
+}
+
+function main() {
+  gzip()
+  brotli()
+}
+
+main()
