805f0f16c9be84e03ffe5d7055321c4cbe47fb71
Sondre Nilsen <nilsen.sondre@gmail.com>
2021-05-14 15:34:19 +0200

    Add a simple navbar, import Tailwind

--- components/layout.tsx
+++ components/layout.tsx
@@ -1,6 +1,7 @@
 import Head from "next/head"
-import Link from "next/link"
 import { ReactNode } from "react"
+
+import { Navbar } from "./navbar"
 
 type Props = {
   children?: ReactNode
@@ -14,22 +15,7 @@ export const Layout = ({ children, title = "This is the default title" }: Props)
       <meta charSet="utf-8" />
       <meta name="viewport" content="initial-scale=1.0, width=device-width" />
     </Head>
-    <header>
-      <nav>
-        <Link href="/">
-          <a>Home</a>
-        </Link>{" "}
-        |{" "}
-        <Link href="/about">
-          <a>About</a>
-        </Link>{" "}
-        |{" "}
-        <Link href="/users">
-          <a>Users List</a>
-        </Link>{" "}
-        | <a href="/api/users">Users API</a>
-      </nav>
-    </header>
+    <Navbar />
     {children}
     <footer>
       <hr />
---
+++ pages/_app.tsx
@@ -0,0 +1,9 @@
+import "tailwindcss/tailwind.css"
+
+import { AppProps } from "next/app"
+
+const App = ({ Component, pageProps }: AppProps): JSX.Element => {
+  return <Component {...pageProps} />
+}
+
+export default App
