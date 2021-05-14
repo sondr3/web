a8a059bb2a7a2934e0794d1d09b11f1747b98a01
Sondre Nilsen <nilsen.sondre@gmail.com>
2021-05-14 15:24:50 +0200

    Add TailwindCSS

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  mode: "jit",
+  purge: ["./pages/**/*.tsx", "./components/**/*.tsx"],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
