b836f0ecd00f391ae171dfb1cfa73944cde95297
Sondre Nilsen <nilsen.sondre@gmail.com>
2021-05-15 18:35:14 +0200

    Refactor navigation and footer to use actual links

---
+++ utils/index.ts
@@ -0,0 +1,3 @@
+export * from "./navigation"
+export * from "./sample-data"
+export * from "./socials"
---
+++ utils/navigation.ts
@@ -0,0 +1,15 @@
+interface NavItem {
+  name: string
+  href: string
+}
+
+export const navigation: NavItem[] = [
+  {
+    name: "About",
+    href: "/about/",
+  },
+  {
+    name: "Posts",
+    href: "/posts/",
+  },
+]
