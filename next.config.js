e84fc8904de2dcac8f6882e3a8e7999db0f5b0d2
Sondre Nilsen <nilsen.sondre@gmail.com>
2021-05-15 20:42:35 +0200

    Make website fully PWA

--- next.config.js
+++ next.config.js
@@ -1,7 +1,15 @@
-module.exports = {
+const withPWA = require("next-pwa")
+const runtimeCaching = require("next-pwa/cache")
+
+module.exports = withPWA({
   reactStrictMode: true,
   trailingSlash: true,
   future: {
     webpack5: true,
   },
-}
+  pwa: {
+    dest: "public",
+    disable: process.env.NODE_ENV === "development",
+    runtimeCaching,
+  },
+})
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -13,6 +13,15 @@ const App = ({ Component, pageProps }: AppProps): JSX.Element => {
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
         <meta httpEquiv="X-UA-Compatible" content="IE=edge,chrome=1" />
+
+        <meta name="application-name" content="Eons :: IO ()" />
+        <meta name="apple-mobile-web-app-capable" content="yes" />
+        <meta name="apple-mobile-web-app-status-bar-style" content="default" />
+        <meta name="apple-mobile-web-app-title" content="Eons :: IO( ()" />
+        <meta name="description" content="The online home for Sondre Nilsen" />
+        <meta name="format-detection" content="telephone=no" />
+        <meta name="mobile-web-app-capable" content="yes" />
+        <meta name="theme-color" content="#ffffff" />
       </Head>
       <DefaultSeo {...defaultSEOConfig} />
       <Component {...pageProps} />
