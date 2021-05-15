3499934ef6272393329eb840919a46c6a8252cc3
Sondre Nilsen <nilsen.sondre@gmail.com>
2021-05-15 18:36:55 +0200

    Open social links in new tabs

--- components/footer.tsx
+++ components/footer.tsx
@@ -15,7 +15,13 @@ export const Footer = (): JSX.Element => {
         </nav>
         <div className="mt-8 flex justify-center space-x-6">
           {socials.map((item) => (
-            <a key={item.name} href={item.href} className="text-gray-400 hover:text-gray-500">
+            <a
+              key={item.name}
+              href={item.href}
+              className="text-gray-400 hover:text-gray-500"
+              rel="noopener noreferrer"
+              target="_blank"
+            >
               <span className="sr-only">{item.name}</span>
               <item.icon className="h-6 w-6" aria-hidden="true" />
             </a>
