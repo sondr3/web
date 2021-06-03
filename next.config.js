75b3d3045e3d74bb502df09e4fd07fe6a6f148af
Sondre Nilsen <nilsen.sondre@gmail.com>
2021-06-03 22:50:12 +0200

    Use VERCEL_ENV for PWA as well

--- next.config.js
+++ next.config.js
@@ -9,7 +9,7 @@ module.exports = withPWA({
   },
   pwa: {
     dest: "public",
-    disable: process.env.NODE_ENV === "development",
+    disable: process.env.VERCEL_ENV === "development",
     runtimeCaching,
   },
 })
