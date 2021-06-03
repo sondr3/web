8882473e194f2b37548a8776ff986ce2c5b9a273
Sondre Nilsen <nilsen.sondre@gmail.com>
2021-06-03 23:25:55 +0200

    Minimize the overall layout of the navbar/landing page

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -4,19 +4,12 @@ import { navigation } from "../utils"
 
 export const Navbar = (): JSX.Element => {
   return (
-    <header className="border-b-4 border-gray-200">
-      <nav className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8" aria-label="Top">
+    <header>
+      <nav className="max-w-4xl mx-auto px-4 sm:px-6 lg:px-8" aria-label="Top">
         <div className="w-full py-2 flex items-center justify-between">
-          <div className="flex items-center lg:w-auto w-full">
+          <div className="flex justify-center lg:justify-between w-full">
             <Link href="/">
-              <a className="mx-auto">
-                <span className="sr-only">Workflow</span>
-                <img
-                  className="h-10 w-auto md:mx-auto"
-                  src="https://tailwindui.com/img/logos/workflow-mark.svg?color=black"
-                  alt=""
-                />
-              </a>
+              <a className="hover:text-indigo-500 uppercase">Home</a>
             </Link>
             <div className="hidden ml-10 space-x-8 lg:block">
               {navigation.map((link) => (
@@ -29,9 +22,11 @@ export const Navbar = (): JSX.Element => {
         </div>
         <div className="py-4 flex flex-wrap justify-center space-x-6 lg:hidden">
           {navigation.map((link) => (
-            <a key={link.name} href={link.href} className="text-base font-medium text-black hover:text-indigo-500">
-              {link.name}
-            </a>
+            <Link href={link.href} key={link.name}>
+              <a key={link.name} href={link.href} className="text-base font-medium text-black hover:text-indigo-500">
+                {link.name}
+              </a>
+            </Link>
           ))}
         </div>
       </nav>
--- pages/index.tsx
+++ pages/index.tsx
@@ -6,29 +6,18 @@ const frontMatter = {
 
 const IndexPage = (): JSX.Element => (
   <Layout frontMatter={frontMatter}>
-    <div className="relative pt-4">
-      <main className="lg:relative">
-        <div className="mx-auto max-w-7xl w-full pt-16 pb-10 text-center lg:py-48 lg:text-left">
-          <div className="px-4 lg:w-1/2 sm:px-8 xl:pr-16">
-            <h1 className="text-4xl tracking-tight font-extrabold text-gray-900 sm:text-5xl md:text-6xl lg:text-5xl xl:text-6xl">
-              <span className="block xl:inline">Hello! I&apos;m</span>{" "}
-              <span className="block text-indigo-600 xl:inline">Sondre Nilsen</span>
-            </h1>
-            <p className="mt-3 max-w-md mx-auto text-lg text-gray-500 sm:text-xl md:mt-5 md:max-w-3xl">
-              I am a full time nerd with a passion for programming languages, mechanical keyboards, hoarding
-              sideprojects and occasionally creating useful software.
-            </p>
-          </div>
-        </div>
-        <div className="relative w-full h-64 sm:h-72 md:h-96 lg:absolute lg:inset-y-0 lg:right-12 lg:w-1/2 lg:h-full">
-          <img
-            className="absolute inset-0 w-full h-full object-cover lg:rounded-full"
-            src="/images/pixel_me.png"
-            alt="Pixelized drawn avatar of Sondre Nilsen"
-          />
-        </div>
-      </main>
-    </div>
+    <main className="py-16">
+      <section className="max-w-3xl mx-auto px-8">
+        <h1 className="text-4xl tracking-tight font-extrabold text-gray-900 sm:text-5xl md:text-6xl lg:text-5xl xl:text-6xl">
+          <span className="inline-block">Hello! I&apos;m</span>{" "}
+          <span className="inline-block text-indigo-600">Sondre</span>
+        </h1>
+        <p className="mt-3 text-lg text-gray-500 sm:text-xl md:mt-5 md:max-w-3xl">
+          I am a full time nerd with a passion for programming languages, mechanical keyboards, hoarding sideprojects
+          and occasionally creating useful software.
+        </p>
+      </section>
+    </main>
   </Layout>
 )
 
