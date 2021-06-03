a9c6e5b1c246e2c708e4dbc66ec7094223d52d39
Sondre Nilsen <nilsen.sondre@gmail.com>
2021-06-03 23:31:16 +0200

    Change indentation of navbar on mobile devices

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -5,13 +5,13 @@ import { navigation } from "../utils"
 export const Navbar = (): JSX.Element => {
   return (
     <header>
-      <nav className="max-w-4xl mx-auto px-4 sm:px-6 lg:px-8" aria-label="Top">
+      <nav className="max-w-md md:max-w-4xl mx-auto px-8 py-4" aria-label="Top">
         <div className="w-full py-2 flex items-center justify-between">
-          <div className="flex justify-center lg:justify-between w-full">
+          <div className="flex justify-center sm:justify-between w-full">
             <Link href="/">
               <a className="hover:text-indigo-500 uppercase">Home</a>
             </Link>
-            <div className="hidden ml-10 space-x-8 lg:block">
+            <div className="hidden ml-10 space-x-8 sm:block">
               {navigation.map((link) => (
                 <Link href={link.href} key={link.name}>
                   <a className="text-base font-medium text-black hover:text-indigo-500">{link.name}</a>
@@ -20,7 +20,7 @@ export const Navbar = (): JSX.Element => {
             </div>
           </div>
         </div>
-        <div className="py-4 flex flex-wrap justify-center space-x-6 lg:hidden">
+        <div className="py-4 flex flex-wrap justify-center space-x-6 sm:hidden">
           {navigation.map((link) => (
             <Link href={link.href} key={link.name}>
               <a key={link.name} href={link.href} className="text-base font-medium text-black hover:text-indigo-500">
