8b118f4e33d1515c759dfd93bfd64227b8988e0a
Sondre Nilsen <nilsen.sondre@gmail.com>
2021-06-06 00:37:11 +0200

    Refactor MDX Header component, add styling/rest of h3..h6

--- components/mdx.tsx
+++ components/mdx.tsx
@@ -10,12 +10,20 @@ interface HeaderProps {
   type: "h1" | "h2" | "h3" | "h4" | "h5" | "h6"
 }
 
-const Header: React.FC<HeaderProps> = ({ type, children, ...props }) => React.createElement(type, props, children)
+const Header: React.FC<HeaderProps> = ({ type, ...props }) => {
+  const Tag = type as keyof JSX.IntrinsicElements
+  return <Tag className="text-gray-900 dark:text-gray-300" {...props} />
+}
+
 const Code: React.FC = (props) => <code className="text-gray-900 dark:text-gray-300" {...props} />
 
 export const components: ComponentMap = {
   h1: (props) => <Header type="h1" {...props} />,
   h2: (props) => <Header type="h2" {...props} />,
+  h3: (props) => <Header type="h3" {...props} />,
+  h4: (props) => <Header type="h4" {...props} />,
+  h5: (props) => <Header type="h5" {...props} />,
+  h6: (props) => <Header type="h6" {...props} />,
   code: Code,
   p: Paragraph,
 }
