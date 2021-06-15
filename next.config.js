d6a4e36e155012673b71b19ff35605d6e4aa4837
Sondre Nilsen <nilsen.sondre@gmail.com>
2021-06-15 21:14:14 +0200

    Upgrade to Next@11

--- next.config.js
+++ next.config.js
@@ -4,9 +4,6 @@ const runtimeCaching = require("next-pwa/cache")
 module.exports = withPWA({
   reactStrictMode: true,
   trailingSlash: true,
-  future: {
-    webpack5: true,
-  },
   pwa: {
     dest: "public",
     disable: process.env.VERCEL_ENV === "development",
