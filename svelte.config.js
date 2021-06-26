e4c4a8946e24ecf3137161521c022cec5f0c904f
Sondre Nilsen <nilsen.sondre@gmail.com>
2021-06-26 22:14:20 +0200

    Add SASS for styling

--- svelte.config.js
+++ svelte.config.js
@@ -1,13 +1,29 @@
 import vercel from "@sveltejs/adapter-vercel"
+import path from "path"
 import preprocess from "svelte-preprocess"
+
+const isDev = process.env.NODE_ENV === "development"
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-  preprocess: preprocess(),
-
+  preprocess: [
+    preprocess({
+      scss: {
+        sourceMap: !isDev,
+        prependData: "@use 'src/styles/_variables.scss' as *;",
+      },
+    }),
+  ],
   kit: {
     target: "#svelte",
     adapter: vercel(),
+    vite: {
+      resolve: {
+        alias: {
+          $components: path.resolve("./src/components"),
+        },
+      },
+    },
   },
 }
 
