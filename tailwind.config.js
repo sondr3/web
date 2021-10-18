8e142af10cdd708c6a2052157a6adbe576d57d45
Sondre Nilsen <sondre@eons.io>
2021-10-18 14:36:08 +0200

    Add and configure TailwindCSS again

---
+++ tailwind.config.js
@@ -0,0 +1,26 @@
+ module.exports = {
+  mode: "jit",
+  purge: ["./templates/**/*.html"],
+  darkMode: "class",
+  theme: {
+    extend: {
+      typography: (theme) => ({
+        dark: {
+          css: {
+            color: theme("colors.gray.400"),
+            "h2,h3,h4": {
+              color: theme("colors.gray.300"),
+            },
+            code: { color: theme("colors.gray.300") },
+          },
+        },
+      }),
+    },
+  },
+  variants: {
+    extend: {
+      typography: ["dark"],
+    },
+  },
+  plugins: [require("@tailwindcss/typography")],
+}
