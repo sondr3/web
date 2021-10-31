062fc016db49b1ac15bbf7b21780f8fbfe402dcb
Sondre Nilsen <sondre@eons.io>
2021-10-31 17:25:26 +0100

    Add personal development dependencies

--- next.config.js
+++ next.config.js
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
   reactStrictMode: true,
-}
+  trailingSlash: true,
+  future: {
+    strictPostcssConfiguration: true,
+  },
+};
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,7 @@
-import type { NextPage } from 'next'
+import type { NextPage } from "next";
 
 const Home: NextPage = () => {
-  return (
-    <p>Hello, world!</p>
-  )
-}
+  return <p>Hello, world!</p>;
+};
 
-export default Home
+export default Home;
