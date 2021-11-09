633616247e252fd05eab9d8382174c0651425632
Sondre Nilsen <sondre@eons.io>
2021-11-09 15:02:40 +0100

    Add @sveltejs/adapter-static as build adapter

--- svelte.config.js
+++ svelte.config.js
@@ -1,9 +1,11 @@
+import adapter from "@sveltejs/adapter-static";
 import preprocess from "svelte-preprocess";
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
   preprocess: preprocess(),
   kit: {
+    adapter: adapter(),
     target: "#svelte",
   },
 };
