3dacfdc5748990b087a5158cffaab7e1bc6c9645
Sondre Nilsen <sondre@eons.io>
2021-11-09 22:27:34 +0100

    Add mdsvex for markdown goodies

---
+++ mdsvex.config.js
@@ -0,0 +1,10 @@
+const config = {
+  extensions: [".svelte.md", ".md", ".svx"],
+  smartypants: {
+    dashes: "oldschool",
+  },
+  remarkPlugins: [],
+  rehypePlugins: [],
+};
+
+export default config;
--- svelte.config.js
+++ svelte.config.js
@@ -1,12 +1,16 @@
 import adapter from "@sveltejs/adapter-static";
+import { mdsvex } from "mdsvex";
 import preprocess from "svelte-preprocess";
+import mdsvexConfig from "./mdsvex.config.js";
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-  preprocess: preprocess(),
+  extensions: [".svelte", ...mdsvexConfig.extensions],
+  preprocess: [preprocess(), mdsvex(mdsvexConfig)],
   kit: {
     adapter: adapter(),
     target: "#svelte",
+    trailingSlash: "always",
   },
 };
 
