18a729098fc9fd9f66291d9f047ca9d33912a515
Sondre Nilsen <sondre@eons.io>
2021-11-30 17:23:19 +0100

    Fix more design things

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -3,12 +3,12 @@
 </script>
 
 <header>
-  <h1>EONS</h1>
+  <h1>EONS :: IO ()</h1>
   <nav>
     <ul>
-      <li><a href="/">Projects</a></li>
-      <li><a href="/">Posts</a></li>
-      <li><a href="/">About</a></li>
+      <!-- <li><a href="/">projects</a></li> -->
+      <!-- <li><a href="/">articles</a></li> -->
+      <li><a href="/">about</a></li>
     </ul>
   </nav>
 </header>
@@ -16,28 +16,46 @@
   <slot />
 </main>
 <footer>
-  <p>Hello, world!</p>
+  <p>
+    Content (C) Sondre Nilsen, licensed under <a href="https://creativecommons.org/licenses/by-sa/4.0/">CC BY-SA 4.0</a>
+  </p>
 </footer>
 
 <style lang="scss">
   header {
     display: flex;
-    flex-direction: row;
-    justify-content: space-between;
+    flex-direction: column;
     padding-top: 1rem;
     grid-area: 1 / 2 / 1 / 12;
 
     @media (min-width: 32em) {
       grid-area: 1 / 4 / 1 / 8;
+      flex-direction: row;
+      justify-content: space-between;
     }
+  }
+
+  h1 {
+    font-size: 1.5rem;
+    align-self: center;
+    padding-bottom: 1rem;
+    font-family: monospace;
   }
 
   ul {
     display: flex;
+    gap: 30px;
     flex-direction: row;
+    justify-content: center;
+  }
+
+  li {
+    color: #4b5563;
+    font-size: 1.25rem;
   }
 
   main {
+    padding-top: 1rem;
     grid-area: 2 / 2 / 2 / 12;
 
     @media (min-width: 32em) {
@@ -46,7 +64,9 @@
   }
 
   footer {
-    height: 40px;
+    margin: 0 auto;
+    padding-bottom: 1rem;
+    color: #4b5563;
     grid-area: 3 / 2 / 3 / 12;
 
     @media (min-width: 32em) {
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -4,20 +4,18 @@
 </svelte:head>
 
 <section>
-  <h1 class="hello">
+  <h1>
     <span>Hello! I&apos;m</span>
     <span class="blue"> Sondre</span>
   </h1>
-  <p class="body">
+  <p>
     I am a full time nerd with a passion for programming languages, mechanical keyboards, hoarding sideprojects and
     occasionally creating useful software.
   </p>
 </section>
 
 <style lang="scss">
-  @use "../variables.scss" as v;
-
-  .hello {
+  h1 {
     font-size: 3rem;
     line-height: 2.5rem;
     font-weight: 800;
@@ -27,7 +25,7 @@
     color: rgba(79, 70, 229);
   }
 
-  .body {
+  p {
     margin-top: 0.5rem;
     font-size: 1.5rem;
     color: rgba(107, 114, 128);
