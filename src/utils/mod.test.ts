ef8a1386fe6f1d9d7a32a510d1c5a6671599e6bc
Sondre Nilsen <sondre@eons.io>
2021-12-26 16:51:23 +0100

    Refix Justfile, add some other utilities

---
+++ src/utils/mod.test.ts
@@ -0,0 +1,19 @@
+import { cacheBust, slugify } from "./mod.ts";
+import { assertEquals } from "https://deno.land/std@0.119.0/testing/asserts.ts";
+
+Deno.test("slugify", () => {
+  const tests = [
+    ["Hello, world! It's a glorious", "hello-world-its-a-glorious"],
+    ["this_ IS a % of $dollars", "this-is-a-of-dollars"],
+    ["1 is equal == to ---3", "1-is-equal-to-3"],
+  ];
+
+  tests.forEach((inputs) => {
+    assertEquals(slugify(inputs[0]), inputs[1]);
+  });
+});
+
+Deno.test("cacheBust", async () => {
+  assertEquals(await cacheBust("content", false), "");
+  assertEquals(await cacheBust("content", true), "9a0364b9");
+});
---
+++ src/utils/mod.ts
@@ -0,0 +1,31 @@
+import { crypto } from "https://deno.land/std@0.119.0/crypto/mod.ts";
+
+export * from "./duration.ts";
+
+/**
+ * Convert a title from a page to a slug that can be used in the generated site. Turns
+ * `Hello, world!` into `hello-world`.
+ */
+export const slugify = (title: string): string => {
+  return title
+    .trim()
+    .toLowerCase()
+    .split(" ")
+    .map((w) => w.replace(/[^\d+a-z]+/gi, ""))
+    .join("-")
+    .replace(/--+/g, "-");
+};
+
+/**
+ * Utility to create a hash from the contents of some file.
+ */
+export const cacheBust = async (content: string, production: boolean): Promise<string> => {
+  if (!production) return "";
+
+  const hash = toHexString(await crypto.subtle.digest("MD5", new TextEncoder().encode(content)));
+  return hash.slice(0, 8);
+};
+
+const toHexString = (bytes: ArrayBuffer): string => {
+  return new Uint8Array(bytes).reduce((str, byte) => str + byte.toString(16).padStart(2, "0"), "");
+};
