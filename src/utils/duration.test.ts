083156963b2711b199ffe9436a2796eef00d1e2e
Sondre Nilsen <sondre@eons.io>
2021-12-26 16:33:22 +0100

    Add duration functionality

---
+++ src/utils/duration.test.ts
@@ -0,0 +1,26 @@
+import { prettyPrintDuration } from "./duration.ts";
+import { assertEquals } from "https://deno.land/std@0.119.0/testing/asserts.ts";
+
+Deno.test("PrettyPrintDuration", async (t) => {
+  await t.step("PrettyPrintDuration: pretty formats duration, less than 1 second", () => {
+    const duration = 683;
+    const formatted = prettyPrintDuration(duration);
+
+    assertEquals(typeof formatted, "string");
+    assertEquals(formatted, "683ms");
+  });
+
+  await t.step("PrettyPrintDuration: pretty formats duration, less than 100ms", () => {
+    const duration = 85;
+    const formatted = prettyPrintDuration(duration);
+
+    assertEquals(formatted, "85ms");
+  });
+
+  await t.step("Pretty formats duration, more than 1 second", () => {
+    const duration = 1632;
+    const formatted = prettyPrintDuration(duration);
+
+    assertEquals(formatted, "1s 632ms");
+  });
+});
