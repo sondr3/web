259dea3a7e8533a63f2e635800d78f50d2960219
Sondre Nilsen <sondre@eons.io>
2022-01-07 15:35:31 +0100

    Move to my own testing framework

--- src/cli.test.ts
+++ src/cli.test.ts
@@ -1,20 +1,21 @@
+import { test } from "@sondr3/minitest"
 import { strict as assert } from "node:assert"
 
 import { CLI } from "./cli.js"
 
-export function cliWithoutDefault() {
+test("cli without defaults", () => {
   const cli = new CLI(["/usr/bin/node", "index.js"])
   assert.equal(cli.command, "build")
   assert(!cli.production)
-}
+})
 
-export function cliWithCommand() {
+test("cli with command", () => {
   const cli = new CLI(["/usr/bin/node", "index.js", "clean"])
   assert(!cli.production)
   assert.equal(cli.command, "clean")
-}
+})
 
-export function cliWithProduction() {
+test("cli with production", () => {
   const cli = new CLI(["/usr/bin/node", "index.js", "build", "-p"])
   assert(cli.production)
-}
+})
