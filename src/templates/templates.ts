16d3ac2188027ae8ef33b92278061f8d008078b2
Sondre Nilsen <sondre@eons.io>
2022-01-12 19:34:45 +0100

    Add minification of HTML

--- src/templates/templates.ts
+++ src/templates/templates.ts
@@ -1,7 +1,14 @@
+import { createConfiguration, minify } from "@minify-html/js";
+
 import { Content } from "../build/content.js";
 import { Site } from "../build/site.js";
 import { layout } from "./layout.js";
 import { page } from "./page.js";
+
+const minifyHtml = (html: string, production: boolean): Buffer => {
+  if (!production) return Buffer.from(html);
+  return minify(html, createConfiguration({ minify_js: false, minify_css: false }));
+};
 
 /**
  * Render special pages, i.e. not automatically rendered.
@@ -9,8 +16,8 @@ import { page } from "./page.js";
  * @param site - Site state
  * @param content - Content to print
  */
-export const renderSpecial = (site: Site, content: Content): string => {
-  return layout(content.title(), content.content(), site.style);
+export const renderSpecial = (site: Site, content: Content): Buffer => {
+  return minifyHtml(layout(content.title(), content.content(), site.style), site.config.production);
 };
 
 /**
@@ -19,15 +26,16 @@ export const renderSpecial = (site: Site, content: Content): string => {
  * @param site - Site state
  * @param content - Content to print
  */
-export const renderLayout = (site: Site, content: Content): string => {
+export const renderLayout = (site: Site, content: Content): Buffer => {
+  let res: string;
   switch (content.metadata.layout) {
     case "page":
-      return layout(
-        content.title(),
-        page(content.frontmatter.title, content.content()),
-        site.style,
-      );
+      res = layout(content.title(), page(content.frontmatter.title, content.content()), site.style);
+      break;
     case "post":
-      return layout(content.title(), content.content(), site.style);
+      res = layout(content.title(), content.content(), site.style);
+      break;
   }
+
+  return minifyHtml(res, site.config.production);
 };
