31d461e83875770ddd8a3de26a467c134a493b00
Sondre Nilsen <sondre@eons.io>
2022-02-14 18:45:46 +0100

    Refactor JSON encoding, build pages

--- app/Main.hs
+++ app/Main.hs
@@ -5,18 +5,21 @@
 module Main (main) where
 
 import Control.Applicative (Applicative (liftA2))
+import Control.Lens
 import Control.Monad (void)
-import Data.Aeson (ToJSON (toJSON))
+import Data.Aeson
+import Data.Aeson.Lens
 import Data.Digest.Pure.MD5 (md5)
+import qualified Data.HashMap.Lazy as HML
 import Data.Maybe (isJust)
 import qualified Data.String as BLU
 import qualified Data.Text as T
 import Development.Shake
-import Development.Shake.Classes (Binary)
+import Development.Shake.Classes
 import Development.Shake.FilePath
 import Development.Shake.Forward
 import GHC.Generics (Generic)
-import Slick (compileTemplate', substitute)
+import Slick
 import System.Environment (lookupEnv)
 
 data SiteMeta = SiteMeta
@@ -28,12 +31,12 @@ data SiteMeta = SiteMeta
     themeUrl :: String
   }
   deriving stock (Generic, Eq, Ord, Show)
-  deriving anyclass (ToJSON, Binary)
+  deriving anyclass (ToJSON, FromJSON, Binary)
 
 siteMeta :: String -> String -> SiteMeta
 siteMeta style theme =
   SiteMeta
-    { siteTitle = "Eons :: IO ()",
+    { siteTitle = "=> Eons :: IO ()",
       siteDescription = "The online home for Sondre Nilsen",
       siteAuthor = "Sondre Nilsen",
       baseUrl = "https://www.eons.io/",
@@ -41,11 +44,20 @@ siteMeta style theme =
       themeUrl = theme
     }
 
--- withSiteMeta :: Value -> Value
--- withSiteMeta (Object obj) = Object $ HML.union obj meta
---   where
---     Object meta = toJSON siteMeta
--- withSiteMeta _ = error "Not allowed"
+withSiteMeta :: Value -> Value -> Value
+withSiteMeta (Object obj) (Object meta) = Object $ HML.union obj meta
+withSiteMeta _ _ = error "only add site meta to objects"
+
+data Page = Page
+  { title :: String,
+    description :: String,
+    content :: String,
+    slug :: String,
+    createdAt :: Maybe String,
+    modifiedAt :: Maybe String
+  }
+  deriving stock (Generic, Eq, Ord, Show)
+  deriving anyclass (ToJSON, FromJSON, Binary)
 
 outputFolder :: FilePath
 outputFolder = "./build/"
@@ -55,8 +67,28 @@ siteFolder = "./site/"
 
 buildIndex :: SiteMeta -> Action ()
 buildIndex meta = do
+  let page = Page {title = "Home", description = "The online home for Sondre Nilsen", content = "", slug = "", createdAt = Nothing, modifiedAt = Nothing}
+      indexData = withSiteMeta (toJSON page) (toJSON meta)
   indexT <- compileTemplate' (siteFolder <> "templates/index.html")
-  writeFile' (outputFolder </> "index.html") . T.unpack $ substitute indexT (toJSON meta)
+  writeFile' (outputFolder </> "index.html") . T.unpack $ substitute indexT indexData
+
+buildPages :: SiteMeta -> Action [Page]
+buildPages meta = do
+  pages <- getDirectoryFiles "" [siteFolder </> "pages" </> "*.md"]
+  forP pages (buildPage meta)
+
+buildPage :: SiteMeta -> FilePath -> Action Page
+buildPage meta path = cacheAction ("pages", path) $ do
+  liftIO . putStrLn $ "Rebuilding page " <> path
+  content <- readFile' path
+  page <- markdownToHTML $ T.pack content
+  let pageData = withSiteMeta page (toJSON meta)
+      slug = case pageData ^? key (T.pack "slug") . _String of
+        Just v -> v
+        Nothing -> error "could not get page slug"
+  template <- compileTemplate' (siteFolder </> "templates" </> "page.html")
+  writeFile' (outputFolder </> T.unpack slug </> "index.html") (T.unpack $ substitute template pageData)
+  convert pageData
 
 copyStaticFiles :: Action ()
 copyStaticFiles = do
@@ -69,8 +101,8 @@ compileScss :: Action String
 compileScss = do
   cache $ cmd "pnpx sass" [siteFolder </> "scss" </> "style.scss", siteFolder </> "scss" </> "style.css"]
   css <- readFile' (siteFolder </> "scss" </> "style.css")
-  let hash = take 8 $ show $ md5 (BLU.fromString css)
-      file = "style." <> hash <> ".css"
+  let cssHash = take 8 $ show $ md5 (BLU.fromString css)
+      file = "style." <> cssHash <> ".css"
   Stdout compressed <- cmd "node scripts/css.mjs" [file]
   writeFile' (outputFolder </> file) compressed
   pure file
@@ -79,8 +111,8 @@ compileJs :: Action String
 compileJs = do
   js <- readFile' (siteFolder </> "js" </> "theme.js")
   cache $ cmd "pnpx terser" [siteFolder </> "js" </> "theme.js", "-c", "-m toplevel", "-o", outputFolder </> "theme.js"]
-  let hash = take 8 $ show $ md5 (BLU.fromString js)
-      file = "theme." <> hash <> ".js"
+  let jsHash = take 8 $ show $ md5 (BLU.fromString js)
+      file = "theme." <> jsHash <> ".js"
   writeFile' (outputFolder </> file) js
   pure file
 
@@ -109,6 +141,7 @@ buildRules = do
   copyStaticFiles
   prod <- liftIO isProd
   let meta = siteMeta css js
+  pages <- buildPages meta
   buildIndex meta
   optimizeHTML prod
   compress prod
--- site/pages/about.md
+++ site/pages/about.md
@@ -7,11 +7,11 @@ slug: about
 ---
 
 Hello, I'm Sondre! I'm a master student on my first year studying algorithms at the
-https://www.uib.no/[University of Bergen] with a passion for safe and reliable
+[University of Bergen](https://www.uib.no/) with a passion for safe and reliable
 software, open source and programming. I'm also a firm believer in applying the KISS principle
 and the worse is better philosophy to my projects. I enjoy and use strongly
 typed languages, Linux and mechanical keyboards.
 
-== Contact me
+## Contact me
 
 If you want to reach out, my email is `my-first-name` at this domain.
