8fd734463b6fa3922fdd61d34c2f10b1ed5e3aa9
Sondre Aasemoen <sondre@eons.io>
2022-08-27 18:15:26 +0200

    Start using Tailwind, add SEO stuff to header

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,11 @@
 import { defineConfig } from "astro/config";
-import react from "@astrojs/react";
 import tailwind from "@astrojs/tailwind";
-
 import sitemap from "@astrojs/sitemap";
+import solidJs from "@astrojs/solid-js";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://www.eons.io",
   trailingSlash: "always",
-  integrations: [react(), tailwind(), sitemap()],
+  integrations: [solidJs(), tailwind(), sitemap()],
 });
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,15 @@
+const { fontFamily } = require("tailwindcss/defaultTheme");
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
-}
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    fontFamily: {
+      sans: ["Piazzolla", ...fontFamily.sans],
+      serif: ["Piazzolla", ...fontFamily.serif],
+      mono: ["Inconsolata", ...fontFamily.mono],
+    },
+    extend: {},
+  },
+  plugins: [require("@tailwindcss/typography")],
+};
