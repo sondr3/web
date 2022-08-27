291bd72fb1992adaf1fec22050a104d84a77e03c
Sondre Aasemoen <sondre@eons.io>
2022-08-28 00:28:42 +0200

    Add astro-compressor

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,10 +3,11 @@ import tailwind from "@astrojs/tailwind";
 import sitemap from "@astrojs/sitemap";
 import solidJs from "@astrojs/solid-js";
 import htmlMinifier from "astro-html-minifier";
+import compressor from "astro-compressor";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://www.eons.io",
   trailingSlash: "always",
-  integrations: [solidJs(), tailwind(), sitemap(), htmlMinifier()],
+  integrations: [solidJs(), tailwind(), sitemap(), htmlMinifier(), compressor()],
 });
