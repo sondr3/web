0924be546f8b7f147e8a977dfda2a8ace8022fb2
Sondre Aasemoen <sondre@eons.io>
2022-08-27 18:51:16 +0200

    Create navbar

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -3,6 +3,7 @@ const { fontFamily } = require("tailwindcss/defaultTheme");
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  darkMode: "class",
   theme: {
     fontFamily: {
       sans: ["Piazzolla", ...fontFamily.sans],
