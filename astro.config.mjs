e1a53bae7a02bd72e078450cc5821de7f4601f9d
Sondre Aasemoen <sondre@eons.io>
2022-08-27 17:36:21 +0200

    Add react, tailwind and sitemap integrations

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,12 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
+import react from "@astrojs/react";
+import tailwind from "@astrojs/tailwind";
+
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: "https://www.eons.io",
+  trailingSlash: "always",
+  integrations: [react(), tailwind(), sitemap()],
+});
