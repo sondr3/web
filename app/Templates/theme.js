fd713f4c9fd276111e5a96f55e384e3f61c4eabf
Sondre Aasemoen <sondre@eons.io>
2022-08-27 17:10:15 +0200

    Hello, Astro

--- app/Templates/theme.js
+++
@@ -1,9 +0,0 @@
-const set = (val) => document.documentElement.setAttribute("data-theme", val);
-if (window.localStorage.getItem("theme")) {
-  set(window.localStorage.getItem("theme"));
-} else if (window.matchMedia("(prefers-color-scheme: dark)").matches) {
-  set("dark");
-}
-window
-  .matchMedia("(prefers-color-scheme: dark)")
-  .addEventListener("change", (e) => set(e.matches ? "dark" : "light"));
--- site/projects/Project.dhall
+++
@@ -1,9 +0,0 @@
-let Project
-    : Type
-    = { name : Text
-      , description : Text
-      , technology : List Text
-      , gitHub : Text
-      }
-
-in  Project
