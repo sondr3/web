5ff68c965c5b577531845c0f86e2df57628e3b10
Sondre Aasemoen <sondre@eons.io>
2022-10-01 17:18:56 +0200

    Add @astrojs/prefetch

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,15 +3,15 @@ import tailwind from "@astrojs/tailwind";
 import sitemap from "@astrojs/sitemap";
 import htmlMinifier from "astro-html-minifier";
 import compressor from "astro-compressor";
+import prefetch from "@astrojs/prefetch";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://www.eons.io",
   trailingSlash: "always",
   integrations: [
-    tailwind({
-      config: { applyBaseStyles: false },
-    }),
+    prefetch(),
+    tailwind({ config: { applyBaseStyles: false } }),
     sitemap(),
     htmlMinifier(),
     compressor(),
