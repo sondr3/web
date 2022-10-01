a07117a2f884f66a282999e2db976b26d4ec8ae3
Sondre Aasemoen <sondre@eons.io>
2022-10-01 15:05:44 +0200

    Upgrade to Astro 1.4, remove shamefully-hoist

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,6 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import sitemap from "@astrojs/sitemap";
-import solidJs from "@astrojs/solid-js";
 import htmlMinifier from "astro-html-minifier";
 import compressor from "astro-compressor";
 
@@ -10,7 +9,6 @@ export default defineConfig({
   site: "https://www.eons.io",
   trailingSlash: "always",
   integrations: [
-    solidJs(),
     tailwind({
       config: { applyBaseStyles: false },
     }),
