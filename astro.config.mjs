c86112d067fd633a68fc03e36c5faf4370c96893
Sondre Aasemoen <sondre@eons.io>
2022-10-07 19:06:01 +0200

    Add mdx, update packages, add installing peer-deps back to .npmrc

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,14 +4,20 @@ import sitemap from "@astrojs/sitemap";
 import htmlMinifier from "astro-html-minifier";
 import compressor from "astro-compressor";
 import prefetch from "@astrojs/prefetch";
+import mdx from "@astrojs/mdx";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://www.eons.io",
   trailingSlash: "always",
   integrations: [
+    mdx(),
     prefetch(),
-    tailwind({ config: { applyBaseStyles: false } }),
+    tailwind({
+      config: {
+        applyBaseStyles: false,
+      },
+    }),
     sitemap(),
     htmlMinifier(),
     compressor(),
