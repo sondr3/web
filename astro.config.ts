48e7fcce57b47dcd3939901be3a66c2ab6b3384a
Sondre Aasemoen <sondre@eons.io>
2023-01-27 23:12:51 +0100

    Setup SASS and Stylelint

--- astro.config.ts
+++ astro.config.ts
@@ -1,25 +1,20 @@
 import mdx from "@astrojs/mdx";
 import prefetch from "@astrojs/prefetch";
 import sitemap from "@astrojs/sitemap";
-import tailwind from "@astrojs/tailwind";
 import { defineConfig } from "astro/config";
 import compressor from "astro-compressor";
 import htmlMinifier from "astro-html-minifier";
 
-// https://astro.build/config
 export default defineConfig({
   site: "https://www.eons.io",
   trailingSlash: "always",
-  integrations: [
-    mdx(),
-    prefetch(),
-    tailwind({
-      config: {
-        applyBaseStyles: false,
+  integrations: [mdx(), prefetch(), sitemap(), htmlMinifier(), compressor()],
+  vite: {
+    css: {
+      devSourcemap: true,
+      preprocessorOptions: {
+        scss: { additionalData: `@use "src/styles/variables" as v;` },
       },
-    }),
-    sitemap(),
-    htmlMinifier(),
-    compressor(),
-  ],
+    },
+  },
 });
---
+++ stylelint.config.cjs
@@ -0,0 +1,8 @@
+/** @type {import('stylelint').Config} */
+module.exports = {
+  extends: [
+    "stylelint-config-standard-scss",
+    "stylelint-config-prettier-scss",
+    "stylelint-config-html",
+  ],
+};
