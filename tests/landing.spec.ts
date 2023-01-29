26cc4e981d2ac09f4692a596c884daa6c766aa79
Sondre Aasemoen <sondre@eons.io>
2023-01-29 13:38:24 +0100

    Update tests

--- tests/landing.spec.ts
+++ tests/landing.spec.ts
@@ -3,6 +3,6 @@ import { test, expect } from "@playwright/test";
 test("landing", async ({ page }) => {
   await page.goto("/");
   await expect(page).toHaveTitle("Home => Eons :: IO ()");
-  await expect(page.locator(".text-4xl")).toContainText("Hello! I'm Sondre");
+  await expect(page.locator("#hello")).toContainText("Hello! I'm Sondre");
   await expect(page).toHaveScreenshot({ maxDiffPixelRatio: 0.05 });
 });
