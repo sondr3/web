f0db598704cbf948278dbf6f20f949e49c805b04
Sondre Aasemoen <sondre@eons.io>
2023-03-20 10:17:30 +0100

    Bump dependencies

--- stylelint.config.cjs
+++ stylelint.config.cjs
@@ -1,8 +1,4 @@
 /** @type {import('stylelint').Config} */
 module.exports = {
-  extends: [
-    "stylelint-config-standard-scss",
-    "stylelint-config-prettier-scss",
-    "stylelint-config-html",
-  ],
+  extends: ["stylelint-config-standard-scss", "stylelint-config-html"],
 };
