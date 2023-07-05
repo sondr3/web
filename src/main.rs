e77e1fbb5fc8049e0cc386499b5d1f5ada2854ce
Sondre Aasemoen <sondre@eons.io>
2023-07-05 22:54:22 +0200

    Move to minijinja over Askama

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,13 @@
 use std::{path::Path, process::Command};
 
-use askama::Template;
+use minijinja::{context, path_loader, Environment};
+use once_cell::sync::Lazy;
 
-#[derive(Template)]
-#[template(path = "index.jinja")]
-struct IndexTemplate {
-    title: String,
-    description: String,
-}
+static ENV: Lazy<Environment<'static>> = Lazy::new(|| {
+    let mut env = Environment::new();
+    env.set_loader(path_loader("templates"));
+    env
+});
 
 fn main() {
     let output = Command::new("pnpm")
@@ -16,15 +16,13 @@ fn main() {
         .expect("tailwind failed");
     println!("{}", String::from_utf8(output.stdout).unwrap());
 
-    let index = IndexTemplate {
-        title: "Home".to_string(),
-        description: "Hello, I'm Sondre! I make things.".to_string(),
-    };
+    let index = ENV.get_template("index.jinja").unwrap();
+    let context = context!(title => "Home", description => "Hello, I'm Sondre! I make things.");
 
     let dist = Path::new("dist");
     if !dist.exists() {
         std::fs::create_dir("./dist").unwrap();
     }
 
-    std::fs::write("./dist/index.html", index.render().unwrap()).unwrap();
+    std::fs::write("./dist/index.html", index.render(context).unwrap()).unwrap();
 }
