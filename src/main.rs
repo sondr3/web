3ae0b51df84005eee9d6ddddad91fd75cf10e12f
Sondre Aasemoen <sondre@eons.io>
2023-07-06 12:40:38 +0200

    Add grass for SCSS compilation

--- src/main.rs
+++ src/main.rs
@@ -9,14 +9,21 @@ static ENV: Lazy<Environment<'static>> = Lazy::new(|| {
     env
 });
 
-fn main() {
-    let index = ENV.get_template("index.jinja").unwrap();
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let css = grass::from_path("./src/styles/styles.scss", &grass::Options::default())?;
+    let digest = format!("{:x}", md5::compute(css.as_bytes()));
+    let _hash = digest.split_at(8).0;
+
+    let index = ENV.get_template("index.jinja")?;
     let context = context!(title => "Home", description => "Hello, I'm Sondre! I make things.");
 
     let dist = Path::new("dist");
     if !dist.exists() {
-        std::fs::create_dir("./dist").unwrap();
+        std::fs::create_dir("./dist")?;
     }
 
-    std::fs::write("./dist/index.html", index.render(context).unwrap()).unwrap();
+    std::fs::write("./dist/styles.css", css)?;
+    std::fs::write("./dist/index.html", index.render(context)?)?;
+
+    Ok(())
 }
