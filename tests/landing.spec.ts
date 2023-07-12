85fd273693c34e331f7f673b51ffdb3b7d4376a6
Sondre Aasemoen <sondre@eons.io>
2023-07-12 18:14:01 +0200

    Moar tests!!!11one

--- tests/landing.spec.ts
+++ tests/landing.spec.ts
@@ -6,3 +6,15 @@ test("landing", async ({ page }) => {
   await expect(page.locator("#hello")).toContainText("Hello! I'm Sondre");
   await expect(page).toHaveScreenshot({ maxDiffPixelRatio: 0.05 });
 });
+
+test("about", async ({ page }) => {
+  await page.goto("/about/");
+  await expect(page).toHaveTitle("About me => Eons :: IO ()");
+  await expect(page.locator(".main")).toContainText("About me");
+  await expect(page).toHaveScreenshot({ maxDiffPixelRatio: 0.05 });
+});
+
+test("404", async ({ page }) => {
+  await page.goto("/404/");
+  await expect(page).toHaveTitle("404 => Eons :: IO ()");
+});
