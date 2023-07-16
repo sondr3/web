a41f9aa41167b5078e01197c2af9885f3b015f3e
Sondre Aasemoen <sondre@eons.io>
2023-07-16 15:15:27 +0200

    Refactor static file handler

--- src/server.rs
+++ src/server.rs
@@ -1,18 +1,20 @@
 use std::{
     ffi::OsStr,
     net::{Ipv4Addr, SocketAddr},
+    path::Path,
     str::FromStr,
 };
 
 use anyhow::{anyhow, Result};
-use tiny_http::Request;
+use tiny_http::{Header, Request};
 
-pub fn create_sync(root: &std::path::Path) -> Result<()> {
+pub fn create_sync(root: &Path) -> Result<()> {
     let addr = SocketAddr::from((Ipv4Addr::LOCALHOST, 3000));
     let server = tiny_http::Server::http(addr).map_err(|_| anyhow!("Failed to start server"))?;
+    let root = root.canonicalize()?;
 
     for req in server.incoming_requests() {
-        if let Err(e) = handle_static_file(root, req) {
+        if let Err(e) = handle_static_file(&root, req) {
             tracing::error!("Error handling request: {}", e);
         }
     }
@@ -20,27 +22,22 @@ pub fn create_sync(root: &std::path::Path) -> Result<()> {
     Ok(())
 }
 
-fn handle_static_file(root: &std::path::Path, req: Request) -> Result<()> {
-    let mut req_path = req.url().to_string();
-
-    if let Some(pos) = req_path.rfind('?') {
-        req_path.truncate(pos);
-    }
-
-    let path = root.to_path_buf().join(&req_path[1..]);
+fn handle_static_file(root: &Path, req: Request) -> Result<()> {
+    let path = root.join(&req.url()[1..]);
     let serve_path = if path.is_file() {
         path
     } else {
         path.join("index.html")
     };
 
+    tracing::debug!("Serving {:?}", serve_path);
+
     if serve_path.exists() {
         let content = std::fs::File::open(&serve_path)?;
         let mut res = tiny_http::Response::from_file(content);
         let mime = match_extension(serve_path.extension());
         let content_type = format!("Content-Type: {mime}");
-        let content_type =
-            tiny_http::Header::from_str(&content_type).map_err(|_| anyhow!("Invalid header"))?;
+        let content_type = Header::from_str(&content_type).unwrap();
         res.add_header(content_type);
 
         req.respond(res)?;
@@ -61,23 +58,17 @@ fn match_extension(extension: Option<&OsStr>) -> &'static str {
         Some("gz") => "application/x-gzip",
         Some("html") => "text/html; charset=utf8",
         Some("ico") => "image/x-icon",
-        Some("jpeg") => "image/jpeg",
-        Some("jpg") => "image/jpeg",
-        Some("js") => "application/javascript",
+        Some("jpeg" | "jpg") => "image/jpeg",
+        Some("js" | "cjs" | "mjs") => "application/javascript",
         Some("json") => "application/json",
-        Some("mjs") => "application/javascript",
         Some("mp4") => "video/mp4",
-        Some("mpeg") => "video/mpeg",
-        Some("mpg") => "video/mpeg",
+        Some("mpeg" | "mpg") => "video/mpeg",
         Some("png") => "image/png",
-        Some("sitemap") => "application/xml",
         Some("svg") => "image/svg+xml",
         Some("txt") => "text/plain; charset=utf8",
         Some("woff") => "application/font-woff",
         Some("woff2") => "application/font-woff2",
-        Some("xml") => "application/xml",
-        Some("xsl") => "application/xml",
-        Some("xss") => "application/xml",
+        Some("sitemap" | "xml" | "xsl" | "xss") => "application/xml",
         _ => "application/octet-stream",
     }
 }
