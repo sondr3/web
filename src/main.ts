7cb3738e43877015e4c39d718dddc8b22c049110
Sondre Aasemoen <sondre@eons.io>
2023-09-12 01:26:09 +0200

    Configure CI, add flags to main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,20 +1,52 @@
 import { brotli, gzip } from "./compress.ts";
+import { Args, parse } from "std/flags/mod.ts";
 import { PATHS } from "./constants.ts";
 import { createContext } from "./context.ts";
 import { buildPages, copyPublicFiles, writeAssets } from "./build.ts";
 import { handleHttp } from "./server.ts";
 
+const HELP = `
+web - website generator
+
+Options:
+  -s, --server      Disable dev server
+  -p, --production  Optimize output
+  -v, --verbose     Verbose output
+  -h, --help        This message
+  
+Environment variables:
+  CI,PROD           Optimize output
+`.trim();
+
+interface Flags extends Args {
+  server: boolean;
+  production: boolean;
+  verbose: boolean;
+  help: boolean;
+}
+
+const flags = parse(Deno.args, {
+  boolean: ["server", "production", "verbose", "help"],
+  default: { server: !Deno.env.has("CI"), production: Deno.env.has("CI"), verbose: false, help: false },
+  alias: { s: "server", v: "verbose", p: "production", h: "help" },
+}) as Flags;
+
 try {
   await Deno.remove(PATHS.out, { recursive: true });
 } catch { /* noop */ }
 
-const context = await createContext(PATHS, "dev");
+if (flags.help) {
+  console.log(HELP);
+  Deno.exit(0);
+}
+
+const context = await createContext(PATHS, flags.production ? "prod" : "dev");
 
 await buildPages(context.pages, context.mode, context.assets);
 await writeAssets(context.assets);
 await copyPublicFiles(context.public_files);
 
-if (context.mode === "dev") {
+if (flags.server) {
   const server = Deno.listen({ port: 3000 });
   console.log("File server running on http://localhost:3000/");
 
