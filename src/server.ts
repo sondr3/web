40eb37497756a068fb2a235ce28df70967587e0d
Sondre Aasemoen <sondre@eons.io>
2023-10-03 21:52:14 +0200

    Simplify file server logic

--- src/server.ts
+++ src/server.ts
@@ -1,48 +1,32 @@
 import * as path from "std/path/mod.ts";
-import * as fs from "std/fs/mod.ts";
 import { PATHS } from "./constants.ts";
+import { fileExists } from "./utils.ts";
+
+const respondWithFile = async (req: Deno.RequestEvent, path: string): Promise<boolean> => {
+  if (!(await fileExists(path))) return false;
+  const file = await Deno.open(path, { read: true });
+  const readableStream = file.readable;
+  const response = new Response(readableStream);
+  await req.respondWith(response);
+
+  return true;
+};
 
 export async function handleHttp(conn: Deno.Conn) {
   const httpConn = Deno.serveHttp(conn);
   for await (const requestEvent of httpConn) {
-    // Use the request pathname as filepath
     const url = new URL(requestEvent.request.url);
     const filepath = decodeURIComponent(url.pathname);
 
-    let actualPath = "";
-    try {
-      if (await fs.exists(path.join(PATHS.out, filepath, "index.html"), { isFile: true })) {
-        actualPath = path.join(PATHS.out, filepath, "index.html");
-      }
-    } catch {
-      // noop
-    }
-
-    try {
-      if (await fs.exists(path.join(PATHS.out, filepath), { isFile: true })) {
-        actualPath = path.join(PATHS.out, filepath);
-      }
-    } catch {
-      // noop
-    }
-
-    // Try opening the file
-    let file;
-    try {
-      file = await Deno.open(actualPath, { read: true });
-    } catch {
-      // If the file cannot be opened, return a "404 Not Found" response
-      const notFoundResponse = new Response("404 Not Found", { status: 404 });
-      await requestEvent.respondWith(notFoundResponse);
+    if (await respondWithFile(requestEvent, path.join(PATHS.out, filepath, "index.html"))) {
       continue;
     }
 
-    // Build a readable stream so the file doesn't have to be fully loaded into
-    // memory while we send it
-    const readableStream = file.readable;
+    if (await respondWithFile(requestEvent, path.join(PATHS.out, filepath))) {
+      continue;
+    }
 
-    // Build and send the response
-    const response = new Response(readableStream);
-    await requestEvent.respondWith(response);
+    const notFoundResponse = new Response("404 Not Found", { status: 404 });
+    await requestEvent.respondWith(notFoundResponse);
   }
 }
