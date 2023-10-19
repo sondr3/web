80749a1c96a73239f48aa1495128eb86eede4b21
Sondre Aasemoen <sondre@eons.io>
2023-10-19 19:53:06 +0200

    Minor performance optimization for SSR of React

--- src/render.tsx
+++ src/render.tsx
@@ -1,4 +1,5 @@
-import renderToStringPretty from "preact-ssr";
+import renderToStringPretty from "preact-pretty";
+import { render } from "preact-render";
 import { Content } from "./content.ts";
 import { Page } from "./templates/page.tsx";
 import type { JSX } from "preact";
@@ -6,9 +7,9 @@ import { FourOhFour } from "./templates/404.tsx";
 import { Index } from "./templates/index.tsx";
 import { Site } from "./site.ts";
 
-const renderPretty = (elem: JSX.Element) => {
-  const hello = renderToStringPretty(elem, {}, { pretty: true });
-  return `<!DOCTYPE html>\n${hello}`;
+const renderPretty = (elem: JSX.Element, site: Site) => {
+  const rendered = site.isProd() ? render(elem) : renderToStringPretty(elem, {}, { pretty: true });
+  return `<!DOCTYPE html>\n${rendered}`;
 };
 
 export const renderTemplate = (
@@ -17,10 +18,10 @@ export const renderTemplate = (
 ) => {
   switch (content.frontmatter.layout) {
     case "page":
-      return renderPretty(<Page content={content} site={site} />);
+      return renderPretty(<Page content={content} site={site} />, site);
     case "404":
-      return renderPretty(<FourOhFour content={content} site={site} />);
+      return renderPretty(<FourOhFour content={content} site={site} />, site);
     case "index":
-      return renderPretty(<Index content={content} site={site} />);
+      return renderPretty(<Index content={content} site={site} />, site);
   }
 };
