61775ad8df48b3eb46e6cd246e7bd116bc125635
Sondre Aasemoen <sondre@eons.io>
2023-11-22 01:27:52 +0100

    Template joys

--- src/main/web/app.cljs
+++ src/main/web/app.cljs
@@ -1,21 +1,23 @@
 (ns web.app
   (:require [cljs-node-io.core :as io]
             [clojure.pprint :refer [pprint]]
-            [rum.core :as rum]))
-
-(rum/defc hello []
-  [:div "Hello, world!"])
+            [rum.core :as rum]
+            [web.templates :as templates]))
 
 (defn files []
   (io/file-seq "site/content/pages"))
 
+(defn file-ext [^File f]
+  (.getExt f))
+
 (defn pages []
   (->> (io/file-seq "site/content/pages")
-       (filter #(= (.getExt %) ".dj"))
+       (filter #(= (file-ext %) ".dj"))
        (map #(io/slurp %))))
 
 (defn -main []
   (prn "Hello, world!")
   (prn (files))
   (pprint (pages))
-  (prn (rum/render-static-markup (hello))))
+  (prn (rum/render-static-markup (templates/hello)))
+  (io/spit "test.html" (rum/render-static-markup (templates/layout))))
