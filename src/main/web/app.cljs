d0cf69a5fcb0676740272a8fd55d1cabced5e62a
Sondre Aasemoen <sondre@eons.io>
2023-11-22 01:05:30 +0100

    More experiments

--- src/main/web/app.cljs
+++ src/main/web/app.cljs
@@ -1,9 +1,21 @@
 (ns web.app
-  (:require [rum.core :as rum]))
+  (:require [cljs-node-io.core :as io]
+            [clojure.pprint :refer [pprint]]
+            [rum.core :as rum]))
 
 (rum/defc hello []
   [:div "Hello, world!"])
 
+(defn files []
+  (io/file-seq "site/content/pages"))
+
+(defn pages []
+  (->> (io/file-seq "site/content/pages")
+       (filter #(= (.getExt %) ".dj"))
+       (map #(io/slurp %))))
+
 (defn -main []
   (prn "Hello, world!")
+  (prn (files))
+  (pprint (pages))
   (prn (rum/render-static-markup (hello))))
