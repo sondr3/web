6210b3d3fd6d1a93a547cb7bbf447bb1b3e2f4ea
Sondre Aasemoen <sondre@eons.io>
2024-01-27 13:25:02 +0100

    Last test of CLJS

--- src/main/web/app.cljs
+++ src/main/web/app.cljs
@@ -2,7 +2,10 @@
   (:require [cljs-node-io.core :as io]
             [clojure.pprint :refer [pprint]]
             [rum.core :as rum]
-            [web.templates :as templates]))
+            [web.templates :as templates]
+            [clojure.spec.alpha :as s]))
+
+(s/def :content/type #{:page :post :project})
 
 (defn files []
   (io/file-seq "site/content/pages"))
@@ -19,5 +22,6 @@
   (prn "Hello, world!")
   (prn (files))
   (pprint (pages))
-  (prn (rum/render-static-markup (templates/hello)))
+  (prn (rum/render-static-markup (templates/layout)))
+  (prn (s/conform :content/type :page))
   (io/spit "test.html" (rum/render-static-markup (templates/layout))))
