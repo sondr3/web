f3ef442711bb2d284a893f202859837508f3b670
Sondre Aasemoen <sondre@eons.io>
2024-01-30 23:52:16 +0100

    Use polka over tinyhttp to configure 404

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,11 @@
-import { App } from "@tinyhttp/app";
 import log from "loglevel";
-import sirv from "sirv";
+import fs from "node:fs";
+import sirv, { type Options } from "sirv";
+import polka from "polka";
 import WebSocket, { WebSocketServer } from "ws";
 import { PATHS } from "./constants.js";
 import type { FsEmitter } from "./watcher.js";
+import path from "node:path";
 
 export class Server {
 	private tx: FsEmitter;
@@ -19,8 +21,25 @@ export class Server {
 }
 
 export function httpServer() {
-	log.info("File server running on http://localhost:3000/");
-	new App().use("/", sirv(PATHS.out)).listen(3000);
+	const opts: Options = {
+		gzip: true,
+		brotli: true,
+		dev: true,
+	};
+
+	const onNoMatch: polka.Options["onNoMatch"] = (_req, res) => {
+		const page = fs.readFileSync(path.join(PATHS.out, "404", "index.html"), "utf-8");
+		res.statusCode = 404;
+		res.setHeader("Content-Type", "text/html");
+		res.end(page);
+	};
+
+	return polka({ onNoMatch })
+		.use(sirv(PATHS.out, opts))
+		.listen(3000, (err: unknown) => {
+			if (err) throw err;
+			log.info("server running on http://localhost:3000/");
+		});
 }
 
 export function websocketServer(tx: FsEmitter) {
