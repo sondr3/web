a82702eea6c5b6f611f5d28d25bcda77917c6acb
Sondre Aasemoen <sondre@eons.io>
2025-02-02 13:35:09 +0100

    Add astro-compressor

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,16 @@
 import { defineConfig } from "astro/config";
 
+import compressor from "astro-compressor";
+
 export default defineConfig({
   base: "/",
   site: "https://www.eons.io",
   trailingSlash: "ignore",
   output: "static",
+
   devToolbar: {
     enabled: false,
   },
+
+  integrations: [compressor()],
 });
