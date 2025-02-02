c0e1fb20883b444c44e0f52d6c561dcbdf0aa465
Sondre Aasemoen <sondre@eons.io>
2025-02-02 13:51:11 +0100

    Remove screenshots from E2E tests

--- tests/landing.spec.ts
+++ tests/landing.spec.ts
@@ -4,5 +4,4 @@ test("landing", async ({ page }) => {
   await page.goto("/");
   await expect(page).toHaveTitle("Home => Eons :: IO ()");
   await expect(page.locator("#hello")).toContainText("Hello! I'm Sondre");
-  await expect(page).toHaveScreenshot({ maxDiffPixelRatio: 0.05 });
 });
