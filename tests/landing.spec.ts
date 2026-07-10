6416c062b3c711a4025b514bb64724eee0ec0f68
Sondre Aasemoen <sondre@eons.io>
2026-07-10 18:00:01 +0200

    Yeet out Playwright

--- tests/landing.spec.ts
+++
@@ -1,7 +0,0 @@
-import { test, expect } from "@playwright/test";
-
-test("landing", async ({ page }) => {
-  await page.goto("/");
-  await expect(page).toHaveTitle("Home => Eons :: IO ()");
-  await expect(page.locator("#hello")).toContainText("Hello! I'm Sondre");
-});
