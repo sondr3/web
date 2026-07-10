f89854506d144c64ef2ce53dc0130546768340b7
Sondre Aasemoen <sondre@eons.io>
2026-07-10 17:54:56 +0200

    Swap back to Astro again

---
+++ astro.config.ts
@@ -0,0 +1,14 @@
+import { defineConfig } from "astro/config";
+import compressor from "astro-compressor";
+
+export default defineConfig({
+  base: "/",
+  site: "https://www.eons.io",
+  trailingSlash: "always",
+  output: "static",
+  build: {
+    format: "directory",
+  },
+  compressHTML: true,
+  integrations: [compressor()],
+});
